24b92a4b7e92fe71e00ab4bab6b38abf98678f02
ubaydullo01 <ubaydullomustafaev@gmail.com>
2025-09-05 11:52:52 +0500

    mobile header change

--- src/components/layout/header/mobile/mobile-menu.tsx
+++ src/components/layout/header/mobile/mobile-menu.tsx
@@ -4,6 +4,7 @@ import { useDecodedPathname } from "@/hooks";
 import { useLocale } from "next-intl";
 import { Dispatch, SetStateAction } from "react";
 import { navItems } from "../constants";
+import { useRouter } from "next/navigation";
 
 type TProps = {
   setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
@@ -12,20 +13,25 @@ type TProps = {
 export const MobileMenu = ({ setIsMenuOpen }: TProps) => {
   const { pathname } = useDecodedPathname();
   const locale = useLocale();
+  const router = useRouter();
+
+  const handleButtonClick = (path: string) => {
+    router.push(path);
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="lg:hidden pb-4">
       <div className="flex flex-col space-y-2">
         {navItems.map((page) => (
           <button
             key={page.path}
-            onClick={() => {
-              setIsMenuOpen(false);
-            }}
             className={`text-left py-2 px-4 rounded-lg text-sm transition-colors duration-200 ${
               pathname === page.path
                 ? "bg-blue-50 text-blue-600 font-medium"
                 : "text-gray-600 hover:bg-gray-50"
             }`}
+            onClick={() => handleButtonClick(page.path)}
           >
             {page.name[locale as keyof typeof page.name]}
           </button>
